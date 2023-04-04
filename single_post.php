e6872a93e6ab5325d4ca757a9b121e0ec483d3a4
imamhosen737 <imamhosen737@gmail.com>
2023-04-04 13:05:17 +0600

    modified

---
+++ single_post.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Blog Page</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+	<!-- Fontawsome cdn -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+</head>
+
+<body>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<!-- Container wrapper -->
+		<div class="container-fluid">
+			<!-- Toggle button -->
+			<button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<i class="fas fa-bars"></i>
+			</button>
+
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<!-- Navbar brand -->
+				<a class="navbar-brand mt-2 mt-lg-0" href="#">
+					<img src="./assets/2022-11-27T06-52-05.802Z-logo ostad.png" height="15" alt="MDB Logo" loading="lazy" />
+				</a>
+			</div>
+
+
+			<!-- Right elements -->
+			<div class="d-flex align-items-center">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link" href="home_page.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="blogs.php">Blog</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="contact.php">Contact</a>
+					</li>
+				</ul>
+			</div>
+			<!-- Right elements -->
+		</div>
+		<!-- Container wrapper -->
+	</nav>
+	<!-- Navbar -->
+
+	<div class="container-fluid mt-2">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<div class="card mb-3">
+					<img src="./assets/bangladesh-cricket-team-huddled-in-field-30p5bs75d45wnjb5.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Bangladesh national cricket team</h5>
+						<p class="card-text">The Bangladesh men's national cricket team, popularly known as The Tigers, is administered by the Bangladesh Cricket Board. It is a Full Member of the International Cricket Council with Test, One-Day International and T20 International status.</p>
+						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+	</div>
+
+</body>
+
+</html>
