a7a47ca1379068ff73c4cdaa3ef9668048d6f8a4
imamhosen737 <imamhosen737@gmail.com>
2023-04-04 12:58:44 +0600

    add file

--- home_page.php
+++ home_page.php
@@ -54,18 +54,14 @@
 	 <div
     class="p-5 text-center bg-image"
     style="
-      background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp');
+      background-image: url('./assets/./banner-1082646_1920.jpg');
       height: 400px;
     "
   >
     <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
       <div class="d-flex justify-content-center align-items-center h-100">
         <div class="text-white">
-          <h1 class="mb-3">Heading</h1>
-          <h4 class="mb-3">Subheading</h4>
-          <a class="btn btn-outline-light btn-lg" href="#!" role="button"
-          >Call to action</a
-          >
+          <h1 class="mb-3">Welcome to my blog website..</h1>
         </div>
       </div>
     </div>
@@ -82,8 +78,8 @@
 				<div class="card mb-3">
 					<img src="./assets/bangladesh-cricket-team-huddled-in-field-30p5bs75d45wnjb5.jpg" class="card-img-top" alt="...">
 					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<h5 class="card-title">Bangladesh national cricket team</h5>
+						<p class="card-text">The Bangladesh men's national cricket team, popularly known as The Tigers, is administered by the Bangladesh Cricket Board. It is a Full Member of the International Cricket Council with Test, One-Day International and T20 International status.</p>
 						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 
 						<a href="" class="btn btn-info btn-sm">Read More..</a>
@@ -92,10 +88,10 @@
 			</div>
 			<div class="col-md-4 col-sm-6">
 				<div class="card mb-3">
-					<img src="./assets/bangladesh-cricket-team-huddled-in-field-30p5bs75d45wnjb5.jpg" class="card-img-top" alt="...">
+					<img src="./assets/./221129095912-02-portugal-ronaldo-goal-fernandes-112822.jpg" class="card-img-top" alt="...">
 					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<h5 class="card-title">Cristiano Ronaldo</h5>
+						<p class="card-text">Cristiano Ronaldo dos Santos Aveiro GOIH ComM is a Portuguese professional footballer who plays as a forward for and captains both Saudi Professional League club Al Nassr and the Portugal national team. Wikipedia</p>
 						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 
 						<a href="" class="btn btn-info btn-sm">Read More..</a>
@@ -119,10 +115,10 @@
 		<div class="row">
 			<div class="col-md-4 col-sm-6">
 				<div class="card mb-3">
-					<img src="./assets/bangladesh-cricket-team-huddled-in-field-30p5bs75d45wnjb5.jpg" class="card-img-top" alt="...">
+					<img src="./assets/1524683440.jpg" class="card-img-top" alt="...">
 					<div class="card-body">
-						<h5 class="card-title">Card title</h5>
-						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<h5 class="card-title">Politics of Bangladesh</h5>
+						<p class="card-text">Bangladesh, to the east of India on the Bay of Bengal, is a South Asian country marked by lush greenery and many waterways. Its Padma (Ganges), Meghna and Jamuna rivers create fertile plains, and travel by boat is common.</p>
 						<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
 
 						<a href="" class="btn btn-info btn-sm">Read More..</a>
